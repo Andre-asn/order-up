3b26fb1be0bfbee5bdd394c664c75e7fa7596d5d
Andre-asn <andresanti.asn@gmail.com>
2026-03-25 18:01:25 -0400

    fix(chore): updating css to fix viewport issue of certain elements, as well as adding oritentation warning on mobile devices

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ import { MusicProvider } from './components/MusicContext'
 function App() {
   return (
     <MusicProvider>
+      <div className="orientation-warning">
+        <span className="orientation-warning-icon">📱</span>
+        <p className="orientation-warning-title">Please rotate your device</p>
+        <p className="orientation-warning-subtitle">Order Up! is best played in portrait mode</p>
+      </div>
       <Routes>
         <Route path="/" element={<MainMenu />} />
         <Route path="/tutorial" element={<TutorialPage />} />
